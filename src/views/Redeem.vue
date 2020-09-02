20b9477c2a55859b049be287bf8614e03ac3dbbb
Meapzer <meapzer@gmail.com>
2020-09-02 15:10:20 +0200

    redeem small changes

--- src/views/Redeem.vue
+++ src/views/Redeem.vue
@@ -2,21 +2,27 @@
   <v-container grid-list-md data-aos="fade-in">
     <v-layout row wrap>
       <v-flex xs12 md12>
-        <v-card dark color="#2b463c">
+        <v-card dark color="primary">
           <v-card-title>Welcome to code redeeming interface!</v-card-title>
           <v-card-text>Here you can redeem codes!</v-card-text>
         </v-card>
       </v-flex>
-
-      <v-flex>
-          
-
+      <v-flex xs12 md12>
+        <v-card color="accent">
+            <v-card-title>
+                <v-text-field filled label="Your code here"></v-text-field>
+            </v-card-title>
+            <v-card-actions>
+                <v-btn text class="primary--text">Submit</v-btn>
+            </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+
 export default Vue.extend({
   data: () => ({
     
